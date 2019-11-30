6e80ce1984d3392c8ceeb9087ecebe11b59bdcc3
Volodymyr Skrypnyk <volodimir.skripnik@gmail.com>
2019-11-30 12:04:24 +0200

    intercepter was changed

--- src/app/services/http-interceptor.service.ts
+++ src/app/services/http-interceptor.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class HttpInterceptorService implements HttpInterceptor {
-  private readonly url: string = 'http://localhost:3000';
+  private readonly url: string = '';
   constructor() { }
 
   /**
