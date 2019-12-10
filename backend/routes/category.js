95453c43fee9900aaa92da3cffd33eed7f7e3362
Volodymyr Skrypnyk <volodimir.skripnik@gmail.com>
2019-12-10 19:33:24 +0200

    added routes for categories

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -7,7 +7,7 @@ const Category = require('../models/category');
  * @swagger
  * /categories:
  *    get:
- *      summary: This should return all event categories
+ *      summary: This should return all categories
  */
 
 router.get("/", (req, res) => {
@@ -20,4 +20,87 @@ router.get("/", (req, res) => {
   }
 );
 
+/**
+ * @swagger
+ * paths:
+ *  /categories:
+ *    post
+ *      summary: Add a new category
+ *      requestBody:
+ *        description: category data
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/models/category/Category'
+ *     responses: '201':
+ *       description: Category Created
+ */
+
+router.post("/", (req, res) => {
+  const newCategory = new Category(req.body);
+  newCategory.save((err, category) => {
+    if (err) return res.status(500).send(err);
+    return res.status(201).json({
+      message: "Category was created successfully!",
+      category: category
+    });
+  })
+});
+
+/**
+ * @swagger
+ * path
+ *  /categories:
+ *    delete:
+ *      summary: This should delete category by ID
+ *      parameters:
+ *      - in: query
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: id of category that have to be deleted
+ */
+
+router.delete("/", (req, res) => {
+  Event.findByIdAndRemove(req.query.id, (err, category) => {
+    if (err) return res.status(500).send(err);
+    return res.status(200).json({
+      message: "Category successfully deleted!",
+      id: category._id
+    })
+  })
+});
+
+/**
+ * @swagger
+ * path
+ *  /events:
+ *    put:
+ *      summary: This should update category by ID
+ *      parameters:
+ *      - in: query
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: id of category that have to be deleted
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/models/category/Category'
+ */
+
+router.put("/", (req, res) => {
+  Category.findByIdAndUpdate(req.body._id, req.body, {new: true}, (err, newCategory) => {
+    if (err) return res.status(500).send(err);
+    return res.json({
+      message: "Category was updated successfully!",
+      category: newCategory
+    })
+  })
+});
 module.exports = router;
